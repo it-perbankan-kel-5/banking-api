5026bc716c92cf0973270a6b0dd5180e062dc681
je <envyxbot@gmail.com>
2023-10-08 23:00:49 +0700

    add: route for profile

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,9 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\TestController;
+use App\Http\Controllers\Api\ProfileController;
 use App\Http\Controllers\Api\TransferController;
 use App\Http\Controllers\Api\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,11 +23,15 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [AuthController::class, 'users']);
 
-    Route::get('/user/balance/details', [UserController::class, 'user_balance_details']);
+    Route::get('/user/balance', [UserController::class, 'user_balance_details']);
     Route::get('/user/income/details', [UserController::class, 'get_user_income_details']);
     Route::get('/user/outcome/details', [UserController::class, 'get_user_outcome_details']);
+    Route::get('/user/transactions', [UserController::class, 'get_user_transactions']);
 
     Route::post('/transfer', [TransferController::class, 'transfer']);
+
+    Route::get('/user/profile', [ProfileController::class, 'profile_details']);
+    Route::post('/user/profile/edit', [ProfileController::class, 'edit_profile']);
 
     Route::post('/logout', [AuthController::class, 'logout']);
 });
